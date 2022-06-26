fe67b1fdb3663c2878f1b2336d5df8869bd6d4eb
Faucon Matthias <matthias.faucon@orange.fr>
2022-06-26 02:06:54 +0200

    ajout de la fonctionnalité d'ajout de nouvelles todos
    Ceci est fait dans le controller "TodoController" avec la méthode addTodo

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -21,6 +21,7 @@ class TodoController extends AbstractController
             'correction' => 'corriger mes devoirs'
         ];
         $session->set('todos', $todos);
+        $this->addFlash('info', 'la liste des todos vient d\'être initialisée' );
     }
     // sinon j'affecte les données contenus dans la session à mon tableau de todos et je l'affiche avec twig
     else{
@@ -31,5 +32,35 @@ class TodoController extends AbstractController
         return $this->render('todo/index.html.twig');
     }
 
+    #[Route('/todo/add/{name}/{content}', name: 'todo.add')]
+    public function addTodo(Request $request, $name, $content)
+    {
+        $session = $request->getSession();
 
+        // Vérifier si il y a le tableau de todos dans la session
+        // si oui
+        if($session->has('todos')) {
+            $todos = $session->get('todos');
+            // Vérifier s'il y a une todo avec le même nom
+                if (isset($todos[$name])){
+                    //si oui
+                        // afficher erreur
+                        $this->addFlash('errorExist', "la todo: $name est déjà dans le tableau todos" );
+                } else{
+                    //si non
+                        // ajouter la todo dans le tableau et message de succès 
+                        $todos[$name] = $content;
+                        //message Flash
+                        $this->addFlash('success', 'Ajout de la todo réussi' );
+                        $session->set('todos', $todos);
+                }
+        }
+        //sinon
+        else{
+            // affiche erreur et renvoie vers le controlleur index
+            $this->addFlash('error', 'Aucune liste de todos dans la session existante' );
+            // return $this->redirectToRoute('app_first');
+        }
+        return $this->redirectToRoute('app_todo');
+    }
 }
